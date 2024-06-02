dd3ab0379ff29e23500064d6753fc04a111ca911
Vinh <vinhpham123.np@gmail.com>
2024-06-02 10:55:29 +0700

    create nix flake for development

---
+++ nix-dev/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A nix flake for development";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ...}:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    devShells.x86_64-linux.default =
+      pkgs.mkShell
+        {
+          nativeBuildInputs = with pkgs; [
+          ];
+          shellHook = ''
+            echo "Welcom to default nix development"
+          '';
+        };
+    python-dev = 
+      pkgs.mkShell
+        {
+          nativeBuildInputs = with pkgs; [
+          ];
+          shellHook = ''
+            echo "Welcom to nix python development"
+          '';
+        };
+  };
+}
