32e73e2e44f7c1b0549f154b2cfce527aca8856e
vicyann <vinhpham123.np@gmail.com>
2024-06-06 23:58:24 +0700

    update monitors config

--- home.nix
+++ home.nix
@@ -77,7 +77,6 @@
       recursive = true;
     };
   };
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
