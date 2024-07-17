5a746954f7ff7eee7bd60f37f6f344104f139516
Vinh <vinhpham123.np@gmail.com>
2024-07-17 00:53:20 +0000

    add support docker desktop

--- hosts/kanade/configuration.nix
+++ hosts/kanade/configuration.nix
@@ -15,6 +15,7 @@
   wsl.enable = true;
   wsl.defaultUser = "vicyann";
   wsl.wslConf.network.hostname = "kanade";
+  wsl.docker-desktop.enable = true;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
